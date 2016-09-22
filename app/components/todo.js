9138acfffe397d663f98dce6217c5aea20119dd8
Colton Savage <coltonsavage@Coltons-MacBook-Pro.local>
2016-09-22 11:34:38 -0500

    initial commit

---
+++ app/components/todo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+var Todo = React.createClass({
+  render: function() {
+    var createItem = function(item) {
+      return <li key={item.id}>{item.text}</li>;
+    };
+    return <ul>{this.props.items.map(createItem)}</ul>;
+  }
+});
+
+export default Todo;
